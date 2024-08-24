091ec781d87351a8aa10d91de7cc94ce5bc9e60b
HEMANTHKUMAR <161803346+Hemanth9148536499@users.noreply.github.com>
2024-08-24 22:46:23 +0530

    Delete Todo-Application/GLCA_Aug23_HEMANTHKUMAR directory

--- Todo-Application/GLCA_Aug23_HEMANTHKUMAR/Backend Project Folder/TODO-APP/src/main/java/com/gl/todo/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.gl.todo.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-	
-	public ResourceNotFoundException(String message) {
-		super(message);
-	}
-
-}
